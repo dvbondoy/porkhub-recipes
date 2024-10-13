737db528447af7eeea1be71f4c591ac113db0a4f
dvbondoy <dvbondoy@yahoo.com>
2024-10-13 16:12:56 +0800

    some features

---
+++ contents/list.md
@@ -0,0 +1,7 @@
+<!-- - [Test](../recipe.html?file=test.md{.title}) -->
+<ul id="myUL">
+    <li>
+        <a href="../recipe.html?file=test.md">Test</a>
+    </li>
+    <li><a href="">Hello</a></li>
+</ul>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
- 
+document.addEventListener('DOMContentLoaded', () => {
+    let script1 = document.querySelector('#handlebars1').innerHTML;
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+    let obj = {
+        "therecipe": urlParams.get('file')
+    };
+
+    let templateScript = Handlebars.compile(script1); // returns a function
+    document.body.innerHTML = templateScript(obj);
+});
+
+function filter_list(){
+    console.log("hello filter");
+    var input = document.getElementById('input');
+    input.onkeyup = function () {
+    var filter = input.value.toUpperCase();
+    var lis = document.getElementsByTagName('li');
+        for (var i = 0; i < lis.length; i++) {
+            var name = lis[i].getElementsByClassName('name')[0].innerHTML;
+            if (name.toUpperCase().indexOf(filter) == 0) 
+                lis[i].style.display = 'list-item';
+            else
+                lis[i].style.display = 'none';
+        }
+    }
+}
